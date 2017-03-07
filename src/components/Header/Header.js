daba7d52eb9769be3d6e249af7ffa53625b84b75
Michał Styś <michal.stys@vml.com>
2017-03-07 17:02:21 +0100

    Added link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default (props) => (
     <div>
-        <a href="">See resource on GitHub</a>
+        <a href="https://github.com/mstys/ReactKonvaAnimalsOnBeach">See resource on GitHub</a>
         <h2>Animals on the Beach Game</h2>
         <a href="">Read article</a>
     </div>
